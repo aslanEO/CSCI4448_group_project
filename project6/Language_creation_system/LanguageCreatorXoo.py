7db0797bc670dd36c847d7d92326a96bcc37b897
Ling <ling.liu@colorado.edu>
2019-12-08 22:06:00 -0700

    Update LanguageCreatorXoo.py

--- project6/Language_creation_system/LanguageCreatorXoo.py
+++ project6/Language_creation_system/LanguageCreatorXoo.py
@@ -13,7 +13,7 @@ class LanguageCreatorXoo(LanguageCreator):
 
     def createData(self, sounds, rules):
         """
-
+        :type sounds: str
         :type rules: str
         """
         dataDict = {}
